06251777ba65f500c5dd07536305f391ff60ade5
hugorichard-larvor <hugog2032@gmail.com>
2025-03-20 16:18:52 +0100

    fix: main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,88 @@
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "groupe12_lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambda_policy" {
+  name       = "groupe12_lambda_policy_attachment"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "time_lambda" {
+  function_name = "groupe12_time_lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+
+  filename         = "lambda/lambda.zip"
+  source_code_hash = filebase64sha256("lambda/lambda.zip")
+
+  environment {
+    variables = {
+      TZ = "Europe/Paris"
+    }
+  }
+}
+
+resource "aws_api_gateway_rest_api" "time_api" {
+  name        = "groupe12_time_api"
+  description = "API Gateway pour la Lambda retournant l'heure actuelle"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.time_api.id
+  parent_id   = aws_api_gateway_rest_api.time_api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy_method" {
+  rest_api_id   = aws_api_gateway_rest_api.time_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.time_api.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.time_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_integration.lambda_integration]
+  rest_api_id = aws_api_gateway_rest_api.time_api.id
+  stage_name  = "prod"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.time_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.time_api.execution_arn}/*/*"
+}
+
+output "api_url" {
+  value       = "${aws_api_gateway_deployment.deployment.invoke_url}/prod"
+  description = "URL de l'API déployée"
+}
